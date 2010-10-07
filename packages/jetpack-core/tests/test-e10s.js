6c265b53ff53bfb0d8a0e93f36e760ae3a366906
Atul Varma <varmaa@gmail.com>
2010-10-07 16:00:52 -0700

    Refactored e10s tests.

--- packages/jetpack-core/tests/test-e10s.js
+++ packages/jetpack-core/tests/test-e10s.js
@@ -1,45 +1,52 @@
 var e10s = require('e10s');
 
-exports.testStartMain = function(test) {
-  var actions = [];
-  var expectedActions = [
-    ["log", "hello", "world"]
-  ];
+function makeConsoleTest(options) {
+  return function(test) {
+    var actions = [];
 
-  var fakeConsole = {
-    log: function log(a, b) {
-      actions.push(["log", a, b]);
-      test.assertEqual(JSON.stringify(actions),
-                       JSON.stringify(expectedActions));
-      test.done();
+    function addAction(action) {
+      actions.push(action);
+      test.assertEqual(JSON.stringify(actions[actions.length-1]),
+                       JSON.stringify(options.expect[actions.length-1]));
+
+      // TODO: It'd be nice to keep going w/ the remote process and
+      // make sure it doesn't log more stuff.
+      if (options.expect.length == actions.length)
+        test.done();
     }
+  
+    function msg(name, args) {
+      var action = [name];
+      for (var i = 0; i < args.length; i++)
+        action.push(args[i]);
+      addAction(action);
+    }
+  
+    var fakeConsole = {
+      exception: function(ex) {
+        addAction(["exception", ex.toString()]);
+      }
+    };
+
+    ["log", "warn", "debug", "error", "info"].forEach(function(name) {
+      fakeConsole[name] = function() { msg(name, arguments); };
+    });
+
+    var process = e10s.createProcess({console: fakeConsole});
+    process.sendMessage("startMain", options.main);
+    test.waitUntilDone();
   };
+}
 
-  var process = e10s.createProcess({console: fakeConsole});
-  process.sendMessage("startMain", "e10s-samples/hello-world");
-  test.waitUntilDone();
-};
+exports.testStartMain = makeConsoleTest({
+  main: "e10s-samples/hello-world",
+  expect: [["log", "hello", "world"]]
+});
 
-exports.testStartMainWithNonexistentModuleWorks = function(test) {
-  var actions = [];
-  var expectedActions = [
+exports.testStartMainWithNonexistentModule = makeConsoleTest({
+  main: "nonexistent-module",
+  expect: [
     ["log", "An exception occurred in the child Jetpack process."],
-    ["exception", "Error: Unknown module 'blah'."]
-  ];
-
-  var fakeConsole = {
-    log: function log(msg) {
-      actions.push(["log", msg]);
-    },
-    exception: function exception(e) {
-      actions.push(["exception", e.toString()]);
-      test.assertEqual(JSON.stringify(actions),
-                       JSON.stringify(expectedActions));
-      test.done();
-    }
-  };
-
-  var process = e10s.createProcess({console: fakeConsole});
-  process.sendMessage("startMain", "blah");
-  test.waitUntilDone();
-};
+    ["exception", "Error: Unknown module 'nonexistent-module'."]
+  ]
+});
